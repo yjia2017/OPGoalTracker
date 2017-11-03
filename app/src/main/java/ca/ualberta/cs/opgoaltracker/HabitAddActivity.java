ae11398cd9bea58f8445d1e1c38a35b0d2f423e4
yjia2017 <3120102701@zju.edu.cn>
2017-11-03 16:58:45 -0600

    add toolbar

--- app/src/main/java/ca/ualberta/cs/opgoaltracker/HabitAddActivity.java
+++ app/src/main/java/ca/ualberta/cs/opgoaltracker/HabitAddActivity.java
@@ -2,6 +2,9 @@ package ca.ualberta.cs.opgoaltracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class HabitAddActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class HabitAddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_add);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar4);
+        setSupportActionBar(myToolbar);
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater myMenuInflater = getMenuInflater();
+//        myMenuInflater.inflate(R.menu.my_menu,menu);
+//        return true ;
+//    }
+
 }
